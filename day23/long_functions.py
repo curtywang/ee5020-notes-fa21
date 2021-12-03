4f9c28ec9486eb9f5701b93f7df4f961eefbd1f9
Curtis Wang <curtywang@protonmail.com>
2021-12-02 19:47:24 -0800

    updated notes

--- day23/long_functions.py
+++ day23/long_functions.py
@@ -4,6 +4,10 @@ import time
 
 def really_hard_task(some_parameter: int) -> float:
     result = np.random.random()
-    time.sleep(2.0)
+    some_num = 0
+    start_time = time.perf_counter()
+    while (time.perf_counter() - start_time < 2.0):  # horrible busy wait
+        some_num += 1
     print(f"finished task crunching {some_parameter} with result {result:.2f}")
-    return result
+    total_duration = time.perf_counter() - start_time
+    return total_duration, result
